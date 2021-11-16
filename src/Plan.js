d6a215f214df4e5b5cc38a9f62203d3bf91a29cf
adebadayo <adebayanen@gmail.com>
2021-11-17 00:12:36 +0900

    change class to function

--- src/Plan.js
+++ src/Plan.js
@@ -8,55 +8,53 @@ const UNIT_NUM_IN_SQUARE = 4
 const UNIT_HEIGHT = SQUARE_HEIGHT / UNIT_NUM_IN_SQUARE
 const UNIT_MINUTES = 60 / UNIT_NUM_IN_SQUARE
 
-class Plan extends React.Component {
-  render() {
-    return (
-      <Rnd
-        key={this.props.idx}
-        className="rnd"
-        default={{
-          x: 0,
-          y: (this.props.startHour * SQUARE_HEIGHT) + (this.props.startMinute/UNIT_MINUTES * UNIT_HEIGHT),
-          width: '100%',
-          height: (this.props.minutes / UNIT_MINUTES) * UNIT_HEIGHT,
+export const Plan = (props) => {
+  return (
+    <Rnd
+      key={props.idx}
+      className="rnd"
+      default={{
+        x: 0,
+        y: (props.startHour * SQUARE_HEIGHT) + (props.startMinute/UNIT_MINUTES * UNIT_HEIGHT),
+        width: '100%',
+        height: (props.minutes / UNIT_MINUTES) * UNIT_HEIGHT,
+      }}
+      dragAxis="y"
+      enableResizing={{
+        top: true, right: false, bottom: true, left: false,
+        topRight: false, bottomRight: false, bottomLeft: false, topLeft: false
+      }}
+      bounds="parent"
+      resizeGrid={[0, UNIT_HEIGHT]}
+      dragGrid={[1, UNIT_HEIGHT]}
+      minWidth="20"
+      plankey={props.plankey}
+      style={{zIndex:props.zIndex}}
+      onResizeStart={props.onResizeStart}
+      onResizeStop={props.onResizeStop}
+      onDragStart={props.onDragStart}
+      onDragStop={props.onDragStop}
+    >
+      <textarea
+        className='description'
+        plankey={props.plankey}
+        style={{
+          zIndex:20,
+          height: (props.minutes / UNIT_MINUTES) * UNIT_HEIGHT
         }}
-        dragAxis="y"
-        enableResizing={{
-          top: true, right: false, bottom: true, left: false,
-          topRight: false, bottomRight: false, bottomLeft: false, topLeft: false
-        }}
-        bounds="parent"
-        resizeGrid={[0, UNIT_HEIGHT]}
-        dragGrid={[1, UNIT_HEIGHT]}
-        minWidth="20"
-        plankey={this.props.plankey}
-        style={{zIndex:this.props.zIndex}}
-        onResizeStart={this.props.onResizeStart}
-        onResizeStop={this.props.onResizeStop}
-        onDragStart={this.props.onDragStart}
-        onDragStop={this.props.onDragStop}
+        onKeyUp={props.saveDescription}
+        rows={1}
+        defaultValue={props.description}
+      />
+      <button
+        className='btn brn-light delete-btn'
+        plankey={props.plankey}
+        onClick={props.deletePlan}
       >
-        <textarea
-          className='description'
-          plankey={this.props.plankey}
-          style={{
-            zIndex:20,
-            height: (this.props.minutes / UNIT_MINUTES) * UNIT_HEIGHT
-          }}
-          onKeyUp={this.props.saveDescription}
-          rows={1}
-          defaultValue={this.props.description}
-        />
-        <button
-          className='btn brn-light delete-btn'
-          plankey={this.props.plankey}
-          onClick={this.props.deletePlan}
-        >
-          ✕
-        </button>
-      </Rnd>
-    )
-  }
+        ✕
+      </button>
+    </Rnd>
+  )
 }
 
 export default Plan;
