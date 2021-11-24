afe9337a33757cd907aad4e5174f4f8068286878
adebadayo <adebayanen@gmail.com>
2021-11-25 02:39:50 +0900

    pass test

--- src/TextPlan.test.js
+++ src/TextPlan.test.js
@@ -3,11 +3,11 @@ import { render } from "react-dom";
 import { act } from "react-dom/test-utils";
 import  { container } from './test_common'
 import ListPlan from "../src/ListPlan";
+import TextPlan from "./TextPlan";
 
 test('aaa', () => {
   act(() => {
-    render(<ListPlan text="test"/>, container);
+    render(<TextPlan text="test"/>, container);
   });
-  expect(container.querySelector("label").textContent).toBe("スケジュール");
   expect(container.querySelector("pre").textContent).toBe("test");
 })
